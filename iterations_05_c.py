0cf53ed4ee2ccd35ecc0f5299de81c32b8ad8edc
santosmarcelob <santos.marcelob@gmail.com>
2022-01-20 16:48:20 -0300

    fixing code

--- iterations_05_c.py
+++ iterations_05_c.py
@@ -3,6 +3,5 @@ print("Before:", smallest)
 for itervar in [3, 41, 12, 9, 74, 15]:
     if smallest is None or itervar < smallest:
         smallest = itervar
-        break
     print("Loop:", itervar, smallest)
 print("Smallest:", smallest)
