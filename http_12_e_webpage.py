9675abd4cfb14f27fffb721d3f35464edcbcb624
santosmarcelob <santos.marcelob@gmail.com>
2022-01-27 21:25:03 -0300

    adding new exercises

---
+++ http_12_e_webpage.py
@@ -0,0 +1,6 @@
+#Reading Web Pages
+import urllib.request, urllib.parse, urllib.error
+
+fhand = urllib.request.urlopen('http://www.dr-chuck.com/page1.htm')
+for line in fhand:
+    print(line.decode().strip())
